2ccaa9739a9ae6e940d256488362a4c07e366ba2
Olga Matveeva <matveevaolga2882@gmail.com>
2023-06-28 13:51:07 +0300

    conf

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pymysql
+from config import host, user, password, db_name
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+try:
+    connection = pymysql.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    print("successfully connected...")
+    print("#" * 20)
 
+    try:
+        cursor = connection.cursor()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        #create table
+        with connection.cursor() as cursor:
+            create_table_query = "CREATE TABLE `users`(id int AUTO_INCREMENT," \
+                                 " name varchar(32)," \
+                                 " password varchar(32)," \
+                                 " email varchar(32), PRIMARY KEY (id));"
+            cursor.execute(create_table_query)
+            print("Table created successfully")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # insert data
+        # with connection.cursor() as cursor:
+        #     insert_query = "INSERT INTO `users` (name, password, email) VALUES ('Anna', 'qwerty', 'anna@gmail.com');"
+        #     cursor.execute(insert_query)
+        #     connection.commit()
+
+        # with connection.cursor() as cursor:
+        #     insert_query = "INSERT INTO `users` (name, password, email) VALUES ('Victor', '123456', 'victor@gmail.com');"
+        #     cursor.execute(insert_query)
+        #     connection.commit()
+        #
+        # with connection.cursor() as cursor:
+        #     insert_query = "INSERT INTO `users` (name, password, email) VALUES ('Oleg', '112233', 'olegan@mail.ru');"
+        #     cursor.execute(insert_query)
+        #     connection.commit()
+
+        # with connection.cursor() as cursor:
+        #     insert_query = "INSERT INTO `users` (name, password, email) VALUES ('Oleg', 'kjlsdhfjsd', 'ole2gan@mail.ru');"
+        #     cursor.execute(insert_query)
+        #     connection.commit()
+        #
+        # with connection.cursor() as cursor:
+        #     insert_query = "INSERT INTO `users` (name, password, email) VALUES ('Oleg', '889922', 'olegan3@mail.ru');"
+        #     cursor.execute(insert_query)
+        #     connection.commit()
+
+        # update data
+        # with connection.cursor() as cursor:
+        #     update_query = "UPDATE `users` SET password = 'xxxXXX' WHERE name = 'Oleg';"
+        #     cursor.execute(update_query)
+        #     connection.commit()
+
+        # delete data
+        # with connection.cursor() as cursor:
+        #     delete_query = "DELETE FROM `users` WHERE id = 5;"
+        #     cursor.execute(delete_query)
+        #     connection.commit()
+
+        # drop table
+        # with connection.cursor() as cursor:
+        #     drop_table_query = "DROP TABLE `users`;"
+        #     cursor.execute(drop_table_query)
+
+        # select all data from table
+        # with connection.cursor() as cursor:
+        #     select_all_rows = "SELECT * FROM `users`"
+        #     cursor.execute(select_all_rows)
+        #     # cursor.execute("SELECT * FROM `users`")
+        #     rows = cursor.fetchall()
+        #     for row in rows:
+        #         print(row)
+        #     print("#" * 20)
+
+    finally:
+        connection.close()
+
+except Exception as ex:
+    print("Connection refused...")
+    print(ex)
