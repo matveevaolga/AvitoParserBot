88e550259328378931d0ce4892520903cede865e
Olga Matveeva <matveevaolga2882@gmail.com>
2023-07-02 20:44:50 +0300

    added insert-and-get logic (interaction of functions insert_data and get_data)

--- db_functions.py
+++ db_functions.py
@@ -5,6 +5,11 @@ import base64
 
 
 class Functions:
+    def __init__(self):
+        self.start_getting_data = 1
+        self.connection = Bd()
+        self.cursor = None
+
     @staticmethod
     def create_form(data, category):
         form = f"insert into {category} ({category}_id, title, photo, description, price, location, link) values ("
@@ -24,7 +29,7 @@ class Functions:
         options.add_argument("--headless")
         driver = webdriver.Chrome(options=options, executable_path=r"webdriver\chromedriver.exe")
         driver.maximize_window()
-        driver.implicitly_wait(60)
+        driver.implicitly_wait(20)
         return driver
 
     @staticmethod
@@ -33,46 +38,51 @@ class Functions:
         return cursor.execute(form)
 
     def insert_data(self, category, numb):
+        self.connection.open_connect()
+        self.cursor = self.connection.connect.cursor()
         try:
-            connection = Bd()
-            connection.open_connect()
-            cursor = connection.connect.cursor()
             driver = Functions.get_driver()
-            for i in range(numb):
-                driver.get(f"https://avito.ru/moskva/{category}")
-                parser = Parser(category, driver)
-                data = parser.parse_data()
-                if Functions.exists(cursor, data[f'{category}_id'], category):
-                    print("Has already been added")
-                else:
-                    form = Functions.create_form(data, category)
-                    cursor.execute(form)
-                    print("Successfully inserted")
+            parser = Parser(category, driver)
+            while numb != 0:
+                try:
+                    driver.get(f"https://avito.ru/moskva/{category}")
+                    data = parser.parse_data()
+                    if Functions.exists(self.cursor, data[f'{category}_id'], category):
+                        print("Has already been added", data[f'{category}_id'])
+                    else:
+                        form = Functions.create_form(data, category)
+                        self.cursor.execute(form)
+                        numb -= 1
+                        print(f"Successfully inserted, {numb} left")
+                except Exception as ex:
+                    print(ex)
+            self.connection.connect.commit()
             driver.close()
-            connection.connect.commit()
-            connection.close_connect()
         except Exception as ex:
             print(ex)
 
     def get_data(self, category, numb):
         try:
-            connection = Bd()
-            connection.open_connect()
-            cursor = connection.connect.cursor()
-            cursor.execute(f"show columns from {category}")
-            columns = [info[0] for info in cursor.fetchall()]
+            self.connection.open_connect()
+            self.cursor = self.connection.connect.cursor()
+            self.cursor.execute(f"show columns from {category}")
+            columns = [info[0] for info in self.cursor.fetchall()]
             result = {}
-            id = 1
-            while id <= numb:
+            end_getting_data = self.start_getting_data + numb
+            while self.start_getting_data < end_getting_data:
                 cur = {}
-                form = f"select * from {category} where id = '{id}';"
-                cursor.execute(form)
-                row = list(cursor.fetchone())
-                row[3] = base64.decodebytes(row[3])
-                for value, column in zip(row, columns):
-                    cur[column] = value
-                result[cur[f'{category}_id']] = cur
-                id += 1
+                form = f"select * from {category} where id = '{self.start_getting_data}';"
+                if self.cursor.execute(form):
+                    row = list(self.cursor.fetchone())
+                    row[3] = base64.decodebytes(row[3])
+                    for value, column in zip(row, columns):
+                        cur[column] = value
+                    result[cur[f'{category}_id']] = cur
+                    self.start_getting_data += 1
+                else:
+                    print(f"Need to insert {end_getting_data - self.start_getting_data} more ads")
+                    self.insert_data(category, end_getting_data - self.start_getting_data)
+            self.connection.close_connect()
             return result
         except Exception as ex:
             print(ex)
